d99645133afd4630556be224ab66701fb9145c5f
marco79423 <marco79423@gmail.com>
2019-04-05 18:09:33 +0800

    Force singleton instance

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -10,6 +10,54 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 let mainWindow
 let readingWindow
 
+const lock = app.requestSingleInstanceLock()
+
+if (!lock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+
+  // quit application when all windows are closed
+  app.on('window-all-closed', () => {
+    // on macOS it is common for applications to stay open until the user explicitly quits
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+
+  app.on('activate', () => {
+    // on macOS it is common to re-create a window even after all windows have been closed
+    if (mainWindow === null) {
+      mainWindow = createMainWindow()
+    }
+  })
+
+  app.on('ready', async () => {
+    if (isDevelopment) {
+      const {default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} = require('electron-devtools-installer')
+
+      for (const extension of [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS]) {
+        await installExtension(extension)
+      }
+    }
+    mainWindow = createMainWindow()
+  })
+
+  ipcMain.on('open-reading-page', (event, comicInfoId) => {
+    if (readingWindow == null) {
+      readingWindow = createReadingWindow(comicInfoId)
+    } else {
+      readingWindow.send('comic-info-id-changed', comicInfoId)
+    }
+  })
+}
+
 function createMainWindow() {
   const window = new BrowserWindow({
     icon: path.join(__static, 'icon.ico'),
@@ -56,7 +104,7 @@ function createMainWindow() {
 }
 
 function createReadingWindow(comicInfId) {
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+  const {width, height} = screen.getPrimaryDisplay().workAreaSize
 
   const window = new BrowserWindow({
     icon: path.join(__static, 'icon.ico'),
@@ -88,38 +136,3 @@ function createReadingWindow(comicInfId) {
 
   return window
 }
-
-ipcMain.on('open-reading-page', (event, comicInfoId) => {
-  if (readingWindow == null) {
-    readingWindow = createReadingWindow(comicInfoId)
-  } else {
-    readingWindow.send('comic-info-id-changed', comicInfoId)
-  }
-})
-
-// quit application when all windows are closed
-app.on('window-all-closed', () => {
-  // on macOS it is common for applications to stay open until the user explicitly quits
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  // on macOS it is common to re-create a window even after all windows have been closed
-  if (mainWindow === null) {
-    mainWindow = createMainWindow()
-  }
-})
-
-app.on('ready', async () => {
-  if (isDevelopment) {
-    const {default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS} = require('electron-devtools-installer')
-
-    for (const extension of [REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS]) {
-      await installExtension(extension)
-    }
-  }
-
-  mainWindow = createMainWindow()
-})
