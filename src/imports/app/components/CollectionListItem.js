4076e3a6f1b6ce63bf1ae2dbf30b7d2fa6e51dbe
marco79423 <marco79423@gmail.com>
2019-04-18 21:54:48 +0800

    Improve view of collection page

--- src/imports/app/components/CollectionListItem.js
+++ src/imports/app/components/CollectionListItem.js
@@ -1,17 +1,60 @@
 import * as React from 'react'
 import {createStyles, withStyles} from '@material-ui/core/styles'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
-import ListItemText from '@material-ui/core/ListItemText'
 import OpenInNewIcon from '@material-ui/icons/OpenInNew'
-import IconButton from '@material-ui/core/IconButton'
-import ListItemAvatar from '@material-ui/core/ListItemAvatar'
-import Avatar from '@material-ui/core/Avatar'
+import Card from '@material-ui/core/Card'
+import CardMedia from '@material-ui/core/CardMedia'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import dateformat from 'dateformat'
+import CardActions from '@material-ui/core/CardActions'
+import Button from '@material-ui/core/Button'
 
 const styles = (theme) => createStyles({
-  root: {},
-  progress: {
-    marginRight: theme.spacing.unit * 2
+  root: {
+    padding: theme.spacing.unit,
+    '&:not(:first-child)': {
+      paddingTop: 0
+    }
+  },
+  card: {
+    display: 'flex',
+  },
+  title: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  metadata: {
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    textOverflow: 'ellipsis',
+
+    marginTop: theme.spacing.unit,
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 300,
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 120,
+    height: 150,
+  },
+  summary: {
+    marginTop: theme.spacing.unit,
+    height: 40,
+    textOverflow: 'ellipsis',
+    overflow: 'hidden'
+  },
+  actions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  openReadingPageButton: {
+    marginRight: theme.spacing.unit,
   }
 })
 
@@ -29,19 +72,26 @@ export class CollectionListItem extends React.Component {
 
     return (
       <ListItem className={classes.root}>
-        <ListItemAvatar>
-          <Avatar>
-            <img src={comicInfo.coverDataUrl}/>
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          primary={comicInfo.name}
-        />
-        <ListItemSecondaryAction>
-          <IconButton onClick={this.openReadingPage}>
-            <OpenInNewIcon/>
-          </IconButton>
-        </ListItemSecondaryAction>
+        <Card className={classes.card}>
+          <CardMedia
+            className={classes.cover}
+            image={comicInfo.coverDataUrl}
+            title={comicInfo.name}
+          />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography className={classes.title} noWrap={true} variant="h2">{comicInfo.name}</Typography>
+              <Typography className={classes.metadata} variant="subtitle2" color="textSecondary">
+                最新: {comicInfo.lastUpdatedChapter} ({dateformat(comicInfo.lastUpdatedTime, 'yyyy/mm/dd')})
+              </Typography>
+              <Typography variant="subtitle2">{comicInfo.summary.substring(0, 35)}...</Typography>
+            </CardContent>
+            <CardActions className={classes.actions}>
+              <Button size="small" variant="contained" color="primary" onClick={this.openReadingPage}><OpenInNewIcon
+                className={classes.openReadingPageButton}/>閱讀</Button>
+            </CardActions>
+          </div>
+        </Card>
       </ListItem>
     )
   }
