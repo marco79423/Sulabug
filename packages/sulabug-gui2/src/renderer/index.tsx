b59f286d533807eef07a3eb817e2a72b57d46464
marco79423 <marco79423@gmail.com>
2019-10-26 14:17:44 +0800

    Implement prototype

--- packages/sulabug-gui2/src/renderer/index.tsx
+++ packages/sulabug-gui2/src/renderer/index.tsx
@@ -1,7 +1,80 @@
-import * as React from 'react'
+import React, {useReducer, useEffect} from 'react'
 import * as ReactDOM from 'react-dom'
+import {HashRouter, Link, Route, Switch} from 'react-router-dom'
+import {Redirect} from 'react-router'
+import {ipcRenderer} from 'electron'
+
+
+function managerReducer(state, action) {
+  switch (action.type) {
+    case 'manager/increment-count':
+      return {...state, count: state.count + 1}
+    default:
+      return state
+  }
+}
+
+function Manager() {
+  const [state, dispatch] = useReducer(managerReducer, {count: 0})
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log('state', state)
+      ipcRenderer.send('sulabug-action', {type: 'add-log', data: state.count})
+      dispatch({type: 'manager/increment-count'})
+    }, 1000)
+  }, [state.count])
+
+  return (
+    <div>
+      manager <Link to='/browser'>browser</Link>
+    </div>
+  )
+}
+
+function browserReducer(state, action) {
+  switch (action.type) {
+    case 'add-log':
+      return {...state, logs: [...state.logs, action.data]}
+    default:
+      return state
+  }
+}
+
+function Browser() {
+  const [state, dispatch] = useReducer(browserReducer, {logs: []})
+  useEffect(() => {
+    ipcRenderer.on('sulabug-action', (e, action) => {
+      dispatch(action)
+    })
+  }, [])
+
+  return (
+    <div>
+      browser <Link to={`/manager`}>manager</Link>
+      <ul>
+        {state.logs.map((log, i) => <li key={i}>{log}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <HashRouter>
+        <Switch>
+          <Redirect exact from='/' to='/manager'/>
+          <Route path="/manager" component={Manager}/>
+          <Route path="/browser" component={Browser}/>
+        </Switch>
+      </HashRouter>
+    </div>
+  )
+}
+
 
 ReactDOM.render(
-  <div>hello world 2</div>,
+  <App/>,
   document.getElementById('app'),
 )
